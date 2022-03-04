8b1bfb43fa4b3bee30c11c1ef3979c7b60f06c78
Apricot <tsinhood@gmail.com>
2022-03-04 23:04:00 +0800

    simulate the experiment

---
+++ Geant4Work/ExperSim/Pb8.16/src/B1DetectorConstruction.cc
@@ -0,0 +1,208 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1DetectorConstruction.cc
+/// \brief Implementation of the B1DetectorConstruction class
+
+#include "B1DetectorConstruction.hh"
+
+#include "G4Tubs.hh"
+#include "G4RunManager.hh"
+#include "G4NistManager.hh"
+#include "G4Box.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+#include "G4Sphere.hh"
+#include "G4Trd.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4Isotope.hh"
+#include "G4Element.hh"
+#include "G4Material.hh"
+#include "G4UnitsTable.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1DetectorConstruction::B1DetectorConstruction()
+: G4VUserDetectorConstruction(),
+  fScoringVolume(0)
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1DetectorConstruction::~B1DetectorConstruction()
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* B1DetectorConstruction::Construct()
+{  
+  // Get nist material manager
+  G4NistManager* nist = G4NistManager::Instance();
+
+  G4String name, symbol;
+  G4double a, z, density;
+
+  G4double temperature, pressure;
+  G4UnitDefinition::BuildUnitsTable();
+
+  density = CLHEP::universe_mean_density;
+   //from PhysicalConstants.h
+  pressure = 1.e-19*pascal;
+  temperature = 0.1*kelvin;
+  new G4Material(name="Galactic", z=1., a=1.01*g/mole, density, kStateGas,temperature,pressure);
+
+  // Envelope parameters
+  //
+  G4double env_sizeXY = 30*cm, env_sizeZ = 40*cm;
+  G4Material* env_mat = nist->FindOrBuildMaterial("G4_AIR");
+   
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps = true;
+
+  //     
+  // World
+  //
+  G4double world_sizeXY = 1.2*env_sizeXY;
+  G4double world_sizeZ  = 1.2*env_sizeZ;
+  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+  
+  G4Box* solidWorld =    
+    new G4Box("World",                       //its name
+       0.5*world_sizeXY, 0.5*world_sizeXY, 0.5*world_sizeZ);     //its size
+      
+  G4LogicalVolume* logicWorld =                         
+    new G4LogicalVolume(solidWorld,          //its solid
+                        world_mat,           //its material
+                        "World");            //its name
+                                   
+  G4VPhysicalVolume* physWorld = 
+    new G4PVPlacement(0,                     //no rotation
+                      G4ThreeVector(),       //at (0,0,0)
+                      logicWorld,            //its logical volume
+                      "World",               //its name
+                      0,                     //its mother  volume
+                      false,                 //no boolean operation
+                      0,                     //copy number
+                      checkOverlaps);        //overlaps checking
+                     
+  //     
+  // Envelope
+  //  
+  G4Box* solidEnv =    
+    new G4Box("Envelope",                    //its name
+        0.5*env_sizeXY, 0.5*env_sizeXY, 0.5*env_sizeZ); //its size
+      
+  G4LogicalVolume* logicEnv =                         
+    new G4LogicalVolume(solidEnv,            //its solid
+                        env_mat,             //its material
+                        "Envelope");         //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    G4ThreeVector(),         //at (0,0,0)
+                    logicEnv,                //its logical volume
+                    "Envelope",              //its name
+                    logicWorld,              //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+ 
+
+  //     
+  // Shape 1 Pb_shielding
+  //  
+  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_Pb");
+  G4ThreeVector pos1 = G4ThreeVector();
+
+  // Conical section shape
+  G4double shape1_sizeXY = 20*cm;
+  G4double shape1_sizeZ  = 8.16*mm;
+  
+  G4Box* solidShape1 =    
+    new G4Box("Shape1",                       //its name
+       0.5*shape1_sizeXY, 0.5*shape1_sizeXY, 0.5*shape1_sizeZ);     //its size
+  
+  G4LogicalVolume* logicShape1 =                         
+    new G4LogicalVolume(solidShape1,         //its solid
+                        shape1_mat,          //its material
+                        "Shape1");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos1,                    //at position
+                    logicShape1,             //its logical volume
+                    "Shape1",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+
+  //     
+  // Shape 2 NaI as detector
+  //
+  G4Material* shape2_mat = nist->FindOrBuildMaterial("Galactic"); //NaI
+  G4ThreeVector pos2 = G4ThreeVector(0, 0, 0.375 * env_sizeZ);
+
+  // Trapezoid shape
+  G4double sizek = 0.067;
+  G4double shape2_pRMin = 0*mm;
+  G4double shape2_pRMax = env_sizeXY * sizek;
+  G4double shape2_pDz = 1*mm;
+  G4double shape2_pSPhi = 0;
+  G4double shape2_pDPhi = 2*CLHEP::pi;
+  G4Tubs* solidShape2 =    
+    new G4Tubs("Shape2",                      //its name
+              shape2_pRMin, shape2_pRMax, 
+              0.5*shape2_pDz, shape2_pSPhi, shape2_pDPhi); //its size
+                
+  G4LogicalVolume* logicShape2 =                         
+    new G4LogicalVolume(solidShape2,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos2,                    //at position
+                    logicShape2,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+
+  // Set Shape2 as scoring volume
+  //
+  fScoringVolume = logicShape2;
+
+  //
+  //always return the physical World
+  //
+  return physWorld;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
