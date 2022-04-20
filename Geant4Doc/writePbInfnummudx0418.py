f186455ea3481a42071d28487614293dd2ac4c6f
apricot1024 <tsinhood@gmail.com>
2022-04-20 11:38:05 +0800

    infPlane infcorrection

---
+++ Geant4Doc/writePbInfnummudx0418.py
@@ -0,0 +1,91 @@
+# 本py脚本用于自动改写B1DetectorConstruction.cc中屏蔽板的厚度
+particleNum = 3000000
+i = 6
+mud = [2,2,2,2,2,2,4,4,4,4,4,4,7,7,7,7,7,7,10,10,10,10,10,10,15,15,15,15,15,15]
+hou = [10.91768611,
+24.81152545,
+38.25693742,
+41.61819881,
+41.98509739,
+37.69228957,
+21.83537221,
+49.6230509,
+76.51387483,
+83.23639761,
+83.97019478,
+75.38457914,
+38.21190137,
+86.84033907,
+133.899281,
+145.6636958,
+146.9478409,
+131.9230135,
+54.58843053,
+124.0576272,
+191.2846871,
+208.090994,
+209.9254869,
+188.4614479,
+81.88264579,
+186.0864409,
+286.9270306,
+312.136491,
+314.8882304,
+282.6921718,
+]
+enerNum = [0,1,2,3,4,5]
+energy = [0.5,1,2,3,4,8]
+
+while i <= 35:
+    j = i % 6
+    f = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/src/B1DetectorConstruction.cc'.format(mud[i-6],enerNum[j]), 'r', encoding='utf-8')
+    new=[]
+    for line in f:
+        new.append(line)
+    f.close()
+    new[63]='  G4double mudh = {};\n'.format(hou[i-6])
+    f = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/src/B1DetectorConstruction.cc'.format(mud[i-6],enerNum[j]), 'w', encoding='utf-8')
+    for n in new:
+        f.write(n)
+    f.close()
+
+    f1 = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/build/rundemo.mac'.format(mud[i-6],enerNum[j]), 'r', encoding='utf-8')
+    new1=[]
+    for line in f1:
+        new1.append(line)
+    f1.close()
+    new1[13]='/gun/energy {} MeV\n'.format(energy[j])
+    new1[15]='/run/beamOn {}\n'.format(particleNum)
+    f1 = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/build/rundemo.mac'.format(mud[i-6],enerNum[j]), 'w', encoding='utf-8')
+    for n in new1:
+        f1.write(n)
+    f1.close()
+
+    # f3 = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/src/B1PrimaryGeneratorAction.cc'.format(mud[i-6],enerNum[j]), 'r', encoding='utf-8')
+    # new3=[]
+    # for line in f3:
+    #     new3.append(line)
+    # f3.close()
+    # new3[83]='  G4double mudh = {};\n'.format(hou[i-6])
+    # f3 = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/src/B1PrimaryGeneratorAction.cc'.format(mud[i-6],enerNum[j]), 'w', encoding='utf-8')
+    # for n in new3:
+    #     f3.write(n)
+    # f3.close()
+
+    f4 = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/src/B1SteppingAction.cc'.format(mud[i-6],enerNum[j]), 'r', encoding='utf-8')
+    new4=[]
+    for line in f4:
+        new4.append(line)
+    f4.close()
+    new4[55]='int eventIDflag[{}] = {{{}}};  //重复计数排除数组\n'.format(particleNum,particleNum+1)
+    new4[56]='int eventIDconvflag[{}] = {{{}}};  // 对发生电子对效应的粒子进行统计\n'.format(particleNum,particleNum+1)
+    new4[97]='      if (E == {})\n'.format(energy[j])
+    new4[104]='      for (int k = 0; k < {}; k++)\n'.format(particleNum)
+    new4[115]='      for (int k = 0; k < {}; k++)\n'.format(particleNum)
+    new4[157]='    if ((eventID == {}) & (outputcode == 10))\n'.format(particleNum-1)
+    f4 = open('/home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/infPlanePb0417/{}mud{}/src/B1SteppingAction.cc'.format(mud[i-6],enerNum[j]), 'w', encoding='utf-8')
+    for n in new4:
+        f4.write(n)
+    f4.close()
+
+    i = i + 1
---
+++ Geant4Work/scatteringRate/PbInf0418/cmakemakerunFor.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#rm -rf ./build/
+#mkdir build
+#cd build/
+mud=(2 2 2 2 2 2 4 4 4 4 4 4 7 7 7 7 7 7 10 10 10 10 10 10 15 15 15 15 15 15)
+for ((i=6; i<=35; i++)); do
+#cmake ..
+#make
+#cp ~/Documents/GraduationProject/Geant4Doc/rundemo1.mac ./
+    b=$(( $i % 6 ))
+    cd /home/apricot/Documents/GraduationProject/Geant4Work/scatteringRate/PbInf0418/${mud[i-6]}mud${b}/build
+    rm CMakeCache.txt
+    rm log*
+    cmake ..
+    make
+    ./exampleB1 rundemo.mac > log${mud[i-6]}mud${b}.txt
+    cp log${mud[i-6]}mud${b}.txt /home/apricot/Documents/GraduationProject/Geant4Work/log/scatteringRate/PbInf0418/
+    echo 'succeed' ${mud[i-6]}'mud'${b}
+done
